84769be96cf4c43663cf7508a604a8cb888f48f8
MaciejRaku <m.rakowski1987@gmail.com>
2017-03-28 12:11:50 +0200

    updating files for new design of smarthome

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function highlightButtonOnDropdownActive() {
+	var activeClass = 'header__nav-item--active';
+	var dropdownButtonSelector = '.header__nav-item--dropdownButton'
+
+	$(dropdownButtonSelector).on('mouseenter', function(){
+		$(this).addClass(activeClass);
+	});
+
+	$('.header__nav-item:not(' + dropdownButtonSelector + ')').on('mouseenter', function(){
+		$(dropdownButtonSelector).removeClass(activeClass);
+	});
+
+	$('.dropdown').on('mouseleave', function() {
+		$(dropdownButtonSelector).removeClass(activeClass);
+	});
+}
+
+$(document).ready(function(){
+	highlightButtonOnDropdownActive();
+});
+
+
+
+$(document).ready(function() {
+    // $(".main").delay(1000).animate({ opacity: 1 }, 1500);
+    $('.main').addClass('fadeIn');
+});
+
+$(document).ready(function() {
+	$("#lightgallery").lightGallery(); 
+});
+
+
+// $(document).ready(function() {
+//     $(".main").delay(2000).fadeIn(500);
+// });
