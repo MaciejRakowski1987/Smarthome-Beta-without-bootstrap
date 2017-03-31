f7c541aa010b41c6eb13e3838077dca83f14c9c7
MaciejRaku <m.rakowski1987@gmail.com>
2017-03-31 12:08:25 +0200

    map add, class rename
    map add /contact

--- js/script.js
+++ js/script.js
@@ -41,6 +41,23 @@ $(document).ready(function(){
 
 });
 
+jQuery(function ($) {
+	function init_map1() {
+    	var myLocation = new google.maps.LatLng(53.4387859, 14.562607299999968);
+        var mapOptions = {
+            center: myLocation,
+            zoom: 16
+        };
+        var marker = new google.maps.Marker({
+            position: myLocation,
+            title: "Property Location"
+        });
+        var map = new google.maps.Map(document.getElementById("map1"),
+            mapOptions);
+            marker.setMap(map);
+        }
+        init_map1();
+    });
 
 // $(document).ready(function() {
 //     $(".main").delay(2000).fadeIn(500);
